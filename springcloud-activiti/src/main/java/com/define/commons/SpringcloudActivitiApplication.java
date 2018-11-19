3253d57402c8bf57889dcd2a810270a8692bd398
林银城 <linyc@gzdefine.com>
2018-11-19 17:30:57 +0800

    优化maven依赖

--- springcloud-activiti/src/main/java/com/define/commons/SpringcloudActivitiApplication.java
+++ springcloud-activiti/src/main/java/com/define/commons/SpringcloudActivitiApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableAutoConfiguration(exclude = {
-		org.activiti.spring.boot.SecurityAutoConfiguration.class,
-})
+@EnableAutoConfiguration(exclude = org.activiti.spring.boot.SecurityAutoConfiguration.class)
 public class SpringcloudActivitiApplication {
 
 	public static void main(String[] args) {
