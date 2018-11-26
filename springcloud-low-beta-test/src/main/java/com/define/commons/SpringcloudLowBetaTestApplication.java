1c5459d193994932253f3ba5d7d9ef899372578f
林银城 <linyc@gzdefine.com>
2018-11-26 18:32:02 +0800

    优化代码

---
+++ springcloud-low-beta-test/src/main/java/com/define/commons/SpringcloudLowBetaTestApplication.java
@@ -0,0 +1,12 @@
+package com.define.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudLowBetaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudLowBetaTestApplication.class, args);
+	}
+}
