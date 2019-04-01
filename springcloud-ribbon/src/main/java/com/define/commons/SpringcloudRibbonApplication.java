a024783bda31fe2ca3971951064a2935db60d907
林银城 <linyc@gzdefine.com>
2019-04-01 20:42:23 +0800

    新增断路器功能

--- springcloud-ribbon/src/main/java/com/define/commons/SpringcloudRibbonApplication.java
+++ springcloud-ribbon/src/main/java/com/define/commons/SpringcloudRibbonApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
+@EnableHystrix
 public class SpringcloudRibbonApplication {
 
 	@Bean
