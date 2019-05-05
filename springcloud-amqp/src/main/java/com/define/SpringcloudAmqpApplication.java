f8eb6b447145abde58569634e23d9f92826efaa2
林银城 <linyc@gzdefine.com>
2019-05-05 09:37:05 +0800

    整合rabbitmq

---
+++ springcloud-amqp/src/main/java/com/define/SpringcloudAmqpApplication.java
@@ -0,0 +1,13 @@
+package com.define;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAmqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudAmqpApplication.class, args);
+	}
+
+}
---
+++ springcloud-amqp/src/test/java/com/define/SpringcloudAmqpApplicationTests.java
@@ -0,0 +1,25 @@
+package com.define;
+
+import com.define.domain.Sender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudAmqpApplicationTests {
+
+	@Autowired
+	private Sender sender;
+
+	public void hello() throws Exception {
+		sender.send();
+	}
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
