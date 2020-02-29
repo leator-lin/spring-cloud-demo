e38c022d298d5f49c6f00423207dc781fef6e0ad
林银城 <linyc@gzdefine.com>
2020-02-29 20:41:24 +0800

    修正项目的路径

---
+++ springcloud-feign-api/src/main/java/com/define/SpringcloudFeignApiApplication.java
@@ -0,0 +1,13 @@
+package com.define;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudFeignApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudFeignApiApplication.class, args);
+	}
+
+}
---
+++ springcloud-feign-api/src/test/java/com/define/SpringcloudFeignApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.define;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudFeignApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
