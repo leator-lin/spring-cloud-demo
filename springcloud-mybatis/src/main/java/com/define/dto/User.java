68fcaaf2c90a304784375b5487857456bb4a7315
leator-lin <c1053595207@163.com>
2020-03-01 20:42:53 +0800

    增加RestTemplate的使用代码

--- springcloud-mybatis/src/main/java/com/define/dto/User.java
+++ springcloud-mybatis/src/main/java/com/define/dto/User.java
@@ -1,12 +1,11 @@
 package com.define.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class User {
-    private String id;
     private String username;
-    private String password;
-    private String address;
-    private String email;
+    private Integer age;
 }
--- springcloud-ribbon/src/main/java/com/define/SpringcloudRibbonApplication.java
+++ springcloud-ribbon/src/main/java/com/define/SpringcloudRibbonApplication.java
@@ -10,13 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * 基于客户端的负载均衡的轮子使用的说明：
- * 1，启动注册中心；
- * 2，用不同的端口启动服务提供者的应用；
- * 3，启动消费者应用：
- * 	3.1，通过@EnableDiscoveryClient注解让该应用注册为
+ * 客户端负载均衡的实现说明：
+ * 1，服务提供者启动多个服务实例；
+ * 2，实现负载均衡最重要的两步，在消费者应用中：
+ * 	2.1，通过@EnableDiscoveryClient注解让该应用注册为
  * 		Eureka客户端应用，以获得服务发现的能力
- * 	3.2，创建RestTemplate的Spring Bean实例，并通过
+ * 	2.2，创建RestTemplate的Spring Bean实例，并通过
  * 		@LoadBalanced注解开启客户端负载均衡
  */
 //使用@EnableCircuitBreaker开启断路器功能
