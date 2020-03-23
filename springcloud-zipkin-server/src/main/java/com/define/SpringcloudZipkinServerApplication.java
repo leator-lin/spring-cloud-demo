b97d344b11f4c69c1beaad7ad4ed71034d9084bc
林银城 <linyc@gzdefine.com>
2020-03-23 16:14:27 +0800

    实现一个Spring Cloud Zipkin链路跟踪项目

--- springcloud-zipkin-server/src/main/java/com/define/SpringcloudZipkinServerApplication.java
+++ springcloud-zipkin-server/src/main/java/com/define/SpringcloudZipkinServerApplication.java
@@ -2,7 +2,9 @@ package com.define;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
 
+@EnableZipkinServer
 @SpringBootApplication
 public class SpringcloudZipkinServerApplication {
 
